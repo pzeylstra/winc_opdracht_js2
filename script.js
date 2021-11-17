42373dc8f1e14b1febf8c476dc9244b5f14d58d2
Paméla Zeylstra <pamela@pam-organisatieondersteuning.nl>
2021-11-17 21:04:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+/* opdracht 2 Javascript part 1*/
+
+// Begroet je medestudenten
+console.log("Hello Winc Academy students")
+
+/* opdracht 2 Javascript part 2*/
+
+// Toon het resultaat van de optelsom
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+
+// Toon het resultaat van de deelsom
+let division = 637263 / 54;
+console.log(division);
+
+// Toon 
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+
+/* In dit voorbeeld wordt 
+de code uitgeschakeld */
+// multiply 637263 * 54;
+let division = 637263 / 54;
+console.log(division);
